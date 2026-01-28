5d134384a78c4e193beb4b5a9372b2f5b21ee9bf
DESKTOP-VTOA1F7\Admin <irsyadkamil96@gmail.com>
2026-01-29 04:29:25 +0800

    Refactor cklistType to use an array of objects for better structure

--- resources/js/src/components/constant.js
+++ resources/js/src/components/constant.js
@@ -1,11 +1,11 @@
 // Constants for the application
 // TODO: Add actual constant values as needed
 
-export const cklistType = {
-    STANDARD: "standard",
-    CUSTOM: "custom",
-    TEMPLATE: "template"
-};
+export const cklistType = [
+    { key: "CHECKLIST", text: "Checklist Item", value: "CHECKLIST" },
+    { key: "LABEL", text: "Label (Info)", value: "LABEL" },
+    { key: "TITLE", text: "Title (Bold)", value: "TITLE" }
+];
 
 export default {
     cklistType
